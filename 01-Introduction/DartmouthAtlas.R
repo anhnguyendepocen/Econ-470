10cbfc1456db5667c87e7ac425c0fe0a18b1d764
Ian McCarthy <ianmccarthy.econ@gmail.com>
2019-10-18 09:20:10 -0400

    Update across computers

---
+++ 01-Introduction/DartmouthAtlas.R
@@ -0,0 +1,40 @@
+
+# Meta --------------------------------------------------------------------
+
+## Title:  Dartmouth Atlas Data
+## Author: Ian McCarthy
+## Date Created: 10/15/2019
+## Date Edited:  10/15/2019
+
+
+# Preliminaries -----------------------------------------------------------
+setwd("C:/users/immccar/CloudStation/Professional/Teaching Material/Emory/Econ 470 - Research in Health Economics and Policy")
+
+## Read in data
+for (t in 2003:2015) {
+  mort.data <- read_xls(paste("data/",t,"_mortality_hrr.xls",sep=""),
+                        col_names=c("HRR","HRR Name","Enrollees",
+                                    "Total_Mortality","NonHMO_Mortality"),
+                        skip=1)
+  mort.data <- mort.data %>%
+    mutate(Year=t)
+  assign(paste("mort.",t,sep=""),mort.data)
+}
+mort.data <- rbind(mort.2003, mort.2004, mort.2005, mort.2006, mort.2007, mort.2008,
+                   mort.2009, mort.2010, mort.2011, mort.2012, mort.2013, mort.2014,
+                   mort.2015)
+exp.data <- read_csv("data/Atlas_Reimbursements_2003-2015.csv")
+
+# Tidy --------------------------------------------------------------------
+
+exp.data <- pivot_longer(exp.data, cols=starts_with("Y"),
+                         names_to="Year", names_prefix="Y",
+                         names_ptypes = list(Year = integer()),
+                         values_to="Expenditures")
+exp.data <- exp.data %>%
+  rename(HRR=HRR_ID) %>%
+  select(HRR, Year, Expenditures)
+
+dartmouth.data <- exp.data %>%
+  left_join(mort.data, by=c("HRR","Year")) %>%
+  filter(complete.cases(.))
