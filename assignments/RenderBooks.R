1964dca3a430f278a7c939ad0bb43c14b1b55c4d
Ian McCarthy <ianmccarthy.econ@gmail.com>
2019-10-25 14:38:58 -0400

    Update readme and first assignment

---
+++ assignments/RenderBooks.R
@@ -0,0 +1,7 @@
+
+
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(tidyverse, ggplot2, dplyr, lubridate, readr, readxl, hrbrthemes,
+               scales, plotly, bookdown)
+
+setwd("D:/CloudStation/Professional/Teaching Material/Emory/Econ 470 - Economics and Health Policy/assignments/")
