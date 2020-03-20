a60fb19aabc2418d624c3be34b709084d5122c91
Ian McCarthy <ianmccarthy.econ@gmail.com>
2020-03-20 10:37:47 -0400

    Update section 4 notes

--- assignments/paths.R
+++ assignments/paths.R
@@ -1,6 +1,7 @@
 path.hwk="D:/CloudStation/Professional/Teaching Material/Emory/Econ 470 - Economics and Health Policy/assignments"
 path.ma.data="D:/CloudStation/Professional/Research Projects/_Git/Medicare-Advantage/data"
 path.hcris.data="D:/CloudStation/Professional/Research Projects/_Git/HCRIS/data"
+path.tobacco.data="D:/CloudStation/Professional/Research Projects/_Git/CDC-Tobacco/data"
 
 #path.hwk="C:/Users/immccar/CloudStation/Professional/Teaching Material/Emory/Econ 470 - Economics and Health Policy/assignments"
 #path.ma.data="C:/Users/immccar/CloudStation/Professional/Research Projects/_Git/Medicare-Advantage/data"
